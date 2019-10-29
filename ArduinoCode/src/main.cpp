d329f2f3227d186bdd45dda8359f265e18ee7cdf
Keller Guillaume <keller.guillaume68@gmail.com>
2019-10-30 00:02:44 +0100

    Check a way to send data type
    Add a code to each kind of data to know what are these and how to use these

--- ArduinoCode/src/main.cpp
+++ ArduinoCode/src/main.cpp
@@ -8,7 +8,6 @@
 
 
 
-
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
@@ -16,7 +15,6 @@
 #define PSTR // Make Arduino Due happy
 #endif
 #include <Fonts/TomThumb.h>
-
 
 #define PIN D5
 #define LED_BRIGHTNESS 32
@@ -31,20 +29,13 @@
     });                                                       \
   }
 
-
 const uint8_t matrixWidth = 32;
 const uint8_t matrixHeight = 8;
 
-
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
-                            NEO_MATRIX_BOTTOM     + NEO_MATRIX_RIGHT +
-                            NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
-                            NEO_GRB            + NEO_KHZ800);
-
-
-
-
-
+                                               NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT +
+                                                   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+                                               NEO_GRB + NEO_KHZ800);
 
 ESP8266WebServer server(80);
 WebSocketsServer webSocket(81);
@@ -52,55 +43,97 @@ WebSocketsServer webSocket(81);
 const size_t capacity = JSON_ARRAY_SIZE(5);
 DynamicJsonDocument doc(capacity);
 
-void waitForWifi() {
+void waitForWifi()
+{
   WiFi.hostname(HOSTNAME);
   WiFi.mode(WIFI_STA);
-  do {
+  do
+  {
     Serial.println(String("Connecting to ") + WIFI_SSID);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
     delay(4000);
   } while (WiFi.status() != WL_CONNECTED);
 }
 
-void handleWebSocket(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght) {
-  int16_t x, y, r, g, b;
-  switch (type) {
-    case WStype_DISCONNECTED:
-      Serial.printf("[%u] Disconnected!\n", num);
-      break;
-    case WStype_TEXT:
-      Serial.printf("[%u] get Text: %s\n", num, payload);
-      // TODO: handle payload via ArduinoJSON
-      deserializeJson(doc, payload);
-      x = (int) doc[0];
-      y = (int) doc[1];  // 3
-      r = (int) doc[2];  // 255
-      g = (int) doc[3];  // 0
-      b = (int) doc[4];  // 0
-      
-      Serial.printf("%s\n",payload);
+void handleWebSocket(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
+{
+  int16_t desc, x, y, r, g, b;
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.printf("[%u] Disconnected!\n", num);
+    break;
+  case WStype_TEXT:
+    Serial.printf("[%u] get Text: %s\n", num, payload);
 
-      Serial.println(x);
-      Serial.println(y);
-      Serial.println(r);
-      Serial.println(g);
-      Serial.println(b);
+    deserializeJson(doc, payload);
+    desc = doc[0];
 
-      matrix.drawPixel(x,y,matrix.Color(r,g,b));
-      
+    switch (desc)
+    {
 
-      
+      //Display Code
+    case 1:
 
+      matrix.show();
 
       break;
-    case WStype_CONNECTED:
-      IPAddress ip = webSocket.remoteIP(num);
-      Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+      //Image Code
+    case 2:
+      x = doc[1];
+      y = doc[2];
+      r = doc[3];
+      g = doc[4];
+      b = doc[5];
+
+      Serial.printf("xy (%d,%d) rgb (%d,%d,%d)", x, y, r, g, b);
+
+      matrix.drawPixel(x, y, matrix.Color(r, g, b));
+
       break;
+
+      //Single pixel Code
+    case 3:
+
+      x = doc[1];
+      y = doc[2];
+      r = doc[3];
+      g = doc[4];
+      b = doc[5];
+
+      Serial.printf("xy (%d,%d) rgb (%d,%d,%d)", x, y, r, g, b);
+
+      matrix.drawPixel(x, y, matrix.Color(r, g, b));
+      matrix.show();
+
+      break;
+
+      //Text Code
+    case 4:
+      std::string txt = doc[1];
+
+      //TODO function to write text
+      
+      break;
+
+    default:
+      Serial.println("Code non reconnu");
+      break;
+    }
+
+    Serial.printf("%s\n", payload);
+
+    break;
+  case WStype_CONNECTED:
+    IPAddress ip = webSocket.remoteIP(num);
+    Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+    break;
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   delay(1000);
@@ -127,44 +160,45 @@ void setup() {
   webSocket.begin();
   webSocket.onEvent(handleWebSocket);
   Serial.println("Websocket Server started");
-/*
+  /*
   initLeds();
   LEDS.addLeds<WS2812, DataPin, GRB>(leds, matrixWidth * matrixHeight);
   LEDS.setBrightness(LED_BRIGHTNESS);
   */
- 
- 
+
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(5);
   matrix.setTextColor(matrix.Color(200, 200, 200));
   matrix.setFont(&TomThumb);
 
-for (int i=0;i<10;i++){
- for (int j=0;j<2;j++){
-  matrix.drawPixel(i,j,matrix.Color(255,255,255));
-  matrix.show();
-  delay(50);
-} 
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
+      matrix.drawPixel(i, j, matrix.Color(255, 255, 255));
+      matrix.show();
+      delay(50);
+    }
+  }
+
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
+      matrix.drawPixel(i, j, matrix.Color(0, 0, 0));
+      matrix.show();
+      delay(50);
+    }
+  }
+
+  Serial.println("Ready");
 }
 
-
-
-for (int i=0;i<10;i++){
- for (int j=0;j<2;j++){
-  matrix.drawPixel(i,j,matrix.Color(0,0,0));
-  matrix.show();
-  delay(50);
-} 
-}
-
- Serial.println("Ready");
-}
-
-void loop() {
+void loop()
+{
   webSocket.loop();
   server.handleClient();
-  matrix.show();
   /*matrix.drawLine(1,2,25,6,White);
 
     matrix.drawRect(31-5,2,5,5,White);
